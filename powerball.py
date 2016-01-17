2e28106ae3953027859cb78612c67809e8c25343
Cong Ma <embolon@gmail.com>
2016-01-17 14:09:02 -0600

    deleting argparser

--- powerball.py
+++ powerball.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python
 
-import argparse
 import random
-
-parser = argparse.ArgumentParser(description='Arguments')
-parser.add_argument('-g', '--gen', help='random generation',
-                    action='store_true', default=True)
-args = parser.parse_args()
-args_var = vars(args)
 
 def randomGen(nP, nR, lP, lR):
     if lP > nP or lR > nR:
@@ -22,7 +15,6 @@ if __name__ == '__main__':
     numRedball = 26
     lengthPowerball = 5
     lengthRedball = 1
-    if args_var['gen']:
-        res = randomGen(numPowerball, numRedball, lengthPowerball, lengthRedball)
-        if res:
-            print('{} {} {} {} {} - {}'.format(res[0], res[1], res[2], res[3], res[4], res[5]))
+    res = randomGen(numPowerball, numRedball, lengthPowerball, lengthRedball)
+    if res:
+        print('{} {} {} {} {} - {}'.format(res[0], res[1], res[2], res[3], res[4], res[5]))
